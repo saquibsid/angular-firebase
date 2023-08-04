0e054d337566b76fdc6bbede70cd86848e2a080a
Mohd Shquib <Mohd.Shquib@igglobal.com>
2023-08-04 12:43:50 +0530

    angular firebase setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:false,
+    firebaseConfig:{
+        apiKey: "AIzaSyC-vG4ZetXEPmzLJxojfumwtdtn0w7nAbM",
+        authDomain: "angularfirebase-37797.firebaseapp.com",
+        databaseURL: "https://angularfirebase-37797-default-rtdb.firebaseio.com",
+        projectId: "angularfirebase-37797",
+        storageBucket: "angularfirebase-37797.appspot.com",
+        messagingSenderId: "244701230597",
+        appId: "1:244701230597:web:950362ceab3d6b7c550e6c"
+    }
+};
